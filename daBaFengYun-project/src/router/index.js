2c2668fde111cd586975d1aa2898d70c34aa6510
付昱萁 <3517802437@qq.com>
2026-04-01 16:37:05 +0800

    继续解决404

--- daBaFengYun-project/src/router/index.js
+++ daBaFengYun-project/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Login from '@/views/login.vue'
 import Home from '@/views/Home.vue'
 
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), 
   routes
 })
 
